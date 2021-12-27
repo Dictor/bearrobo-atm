96f312e78a0d29681a7abcae9dc4ce23c34d54ad
Jeonghyun Kim <kimdictor@gmail.com>
2021-12-27 15:15:48 +0900

    Implement simpleAtmCashbin

---
+++ atmcashbin/go.mod
@@ -0,0 +1,7 @@
+module atmcashbin
+
+go 1.17
+
+require github.com/dictor/bearrobo-atm/atmcontroller v0.0.0
+
+replace github.com/dictor/bearrobo-atm/atmcontroller v0.0.0 => ../atmcontroller
---
+++ atmcashbin/simpleAtmCashbin.go
@@ -0,0 +1,36 @@
+package atmcashbin
+
+import "fmt"
+
+type (
+	SimpleAtmCashbin struct {
+		balance int
+	}
+)
+
+func (cb *SimpleAtmCashbin) Init() error {
+	// in real cash bin, hardward initialize code is wrote here.
+	cb.balance = 1000
+	return nil
+}
+
+func (cb *SimpleAtmCashbin) Balance() int {
+	return cb.balance
+}
+
+func (cb *SimpleAtmCashbin) RecieveCash() (error, int) {
+	fmt.Println("[SimpleAtmCashbin] cashbin opened")
+	fmt.Println("[SimpleAtmCashbin] enter amount of deposit dollars : ")
+	amount := 0
+	fmt.Scanln(&amount)
+	fmt.Println("[SimpleAtmCashbin] cashbin closed")
+	return nil, amount
+}
+
+func (cb *SimpleAtmCashbin) EmitCash(int) error {
+	fmt.Println("[SimpleAtmCashbin] cashbin opened")
+	fmt.Println("[SimpleAtmCashbin] enter after get cash")
+	fmt.Scanln()
+	fmt.Println("[SimpleAtmCashbin] cashbin closed")
+	return nil
+}
