8a6d29cfc0e4e8001f7e727e7c355be347d58011
Jeonghyun Kim <kimdictor@gmail.com>
2021-12-27 15:40:51 +0900

    Implement main function

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"runtime"
+
+	"github.com/dictor/bearrobo-atm/atmcashbin"
+	"github.com/dictor/bearrobo-atm/atmcontroller"
+	"github.com/dictor/bearrobo-atm/atmmodel"
+	"github.com/dictor/bearrobo-atm/atmviewer"
+)
+
+func main() {
+	var (
+		controller atmcontroller.SimpleAtmController = atmcontroller.SimpleAtmController{}
+		viewer     atmcontroller.AtmViewer           = &atmviewer.ConsoleAtmViewer{}
+		model      atmcontroller.AtmModel            = &atmmodel.SimpleAtmModel{}
+		cashbin    atmcontroller.AtmCashBin          = &atmcashbin.SimpleAtmCashbin{}
+	)
+	controller.Init(viewer, model, cashbin)
+
+	for {
+		runtime.Gosched()
+	}
+}
